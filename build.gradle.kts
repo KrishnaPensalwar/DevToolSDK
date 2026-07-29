bb062c880d1ee6cc6168cfeedd1a6641423269c1
KrishnaPensalwar <krishnapensalwar3@gmail.com>
2026-07-30 00:23:46 +0530

    Modified project structure to make all the not required files to keep as private.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,5 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.ksp) apply false
     alias(libs.plugins.kotlin.compose) apply false
-
-    id("com.vanniktech.maven.publish") version "0.34.0" apply false
+    alias(libs.plugins.maven.publish) apply false
 }
